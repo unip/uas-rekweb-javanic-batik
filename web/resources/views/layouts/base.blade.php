70bcc643ed4ad3681a5b6d2b1b22d538e0d829f3
Hanif <abdurrahman.alhanif@gmail.com>
2024-01-03 06:11:19 +0700

    add favicon

--- web/resources/views/layouts/base.blade.php
+++ web/resources/views/layouts/base.blade.php
@@ -4,6 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>{{ $title ? "$title - Javanic Batik" : 'Javanic Batik' }}</title>
+  <link rel="icon" href="/images/favicons/favicon.ico" sizes="any">
+  <link rel="icon" href="/images/favicons/favicon.svg" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="/images/favicons/apple-touch-icon.png">
+  <link rel="manifest" href="/site.webmanifest">
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
   <link rel="stylesheet" href="/css/app.css">
